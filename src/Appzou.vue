94cc5bdd9f6f480457884c28b81d345c42526f90
lss <3104649492@qq.com>
2022-08-25 09:27:59 +0800

    vue-task1

---
+++ src/Appzou.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in Myli" :key="item">{{ item }}</li>
+    </ul>
+    <button @click="walk">按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Myli: ["刘备", "张飞", "关羽"],
+    };
+  },
+  methods: {
+    walk() {
+      this.Myli.push(this.Myli[0]);
+      this.Myli.shift(this.Myli[0]);
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  width: 60px;
+  height: 30px;
+  background-color: cyan;
+}
+</style>
---
+++ src/App随机事件.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in num" :key="item">
+        {{ item }}&nbsp; &nbsp;&nbsp;<button @click="deleted(index)">
+          删除
+        </button>
+      </li>
+    </ul>
+    <button @click="add">增加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: [1, 5, 7],
+    };
+  },
+  methods: {
+    deleted(index) {
+      this.num.splice(index, 1);
+    },
+    add() {
+      this.num.push(Math.random(0, 1).toFixed(2) * 100);
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  width: 60px;
+  height: 30px;
+  background-color: cyan;
+}
+</style>
